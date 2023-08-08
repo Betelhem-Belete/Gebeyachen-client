650f0f64fa285b1b5801ddd2d6b6f918d4b9f1d1
Betelhem-Belete <betelhembelete0@gmail.com>
2023-08-08 12:38:20 -0400

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,13 @@
-import React from "react";
-import Footer from "../components/footer";
-import Navbar from "../components/navbar";
-import "./styles.css";
+import React from 'react';
+import Footer from '../components/footer';
+import Navbar from '../components/navbar';
+import './styles.css';
 function About() {
   return (
     <div>
       <Navbar />
       <div className="content">about</div>
+
       <Footer />
     </div>
   );
